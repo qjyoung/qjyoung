439e41cde9f6897c8f2d98829a102033fe7a2f5a
Jackie Qiao <qjyoung@163.com>
2022-02-20 16:45:10 +0800

    format

---
+++ x.js
@@ -0,0 +1,9 @@
+var _hmt = _hmt || [];
+(function () {
+  let hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9af9c46c970d24ceed26f5283d61aaf0";
+  let s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
+console.log("欢迎关注公众号：程序员阿诺")
